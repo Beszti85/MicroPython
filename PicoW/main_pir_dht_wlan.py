e305257cd2cff03e74b60b537de57efea1f2e845
Beszti85 <csababesztercei@gmail.com>
2023-05-27 13:04:56 +0200

    Added DS1307 driver and I2C for PicoW board

--- PicoW/main_pir_dht_wlan.py
+++ PicoW/main_pir_dht_wlan.py
@@ -1,19 +1,35 @@
 import machine
-from machine import Pin, Timer, ADC, I2C
+from machine import Pin, Timer, ADC, I2C, SPI
 import network
 import utime
 import time
 import dht
+from ds1307 import DS1307
 
 sensor_dht11 = dht.DHT11(machine.Pin(2))
 #led_red = Pin(3, Pin.OUT)
 led = machine.PWM(machine.Pin(3))
 led.freq(1000)
 potentiometer = ADC(28)
-#buzz    = Pin(5, Pin.OUT)
-buzz_pwm = machine.PWM(machine.Pin(5))
+#buzz      = Pin(5, Pin.OUT)
+buzz_pwm   = machine.PWM(machine.Pin(5))
 sensor_pir = Pin(6, Pin.IN, Pin.PULL_DOWN)
 alarm_out  = Pin(7, Pin.OUT)
+i2c_board  = I2C(0, scl = Pin(21), sda = Pin(20), freq = 100000)
+spi_board  = SPI(0, sck = Pin(18), mosi = Pin(19), miso = Pin(16), baudrate = 1000000)
+
+#result = i2c_board.scan()
+#print(result)
+#result = i2c_board.readfrom_mem(0x68, 0x00, 8)
+#print(result)
+datetime_now = [0x00, 0x26, 0x18, 0x04, 0x18, 0x05, 0x23]
+print(datetime_now)
+
+rtc = DS1307(i2c_board)
+#rtc.DateTime(bytearray(datetime_now))
+rtcTime = rtc.DateTime()
+print([hex(x) for x in rtcTime])
+
 timer = Timer()
 tim_buzzer = Timer()
 
@@ -52,19 +68,21 @@ time.sleep(1)
 print(wlan.ifconfig())
 # Handle connection error
 if wlan.status() != 3:
-    raise RuntimeError('network connection failed')
+    print('network connection failed')
 else:
     print('connected')
     status = wlan.ifconfig()
     print( 'ip = ' + status[0] )
     
 while True:
-    time.sleep(5)
+    time.sleep(10)
     sensor_dht11.measure()
     dht11_temp = (sensor_dht11.temperature())
     dht11_hum = (sensor_dht11.humidity())
     read_adctemp = sensor_temp.read_u16() * conversion_factor
     adc_temperature = 27 - (read_adctemp - 0.706)/0.001721
+    print([hex(x) for x in rtc.DateTime()])
+    print(rtc.PrintTime())
     print("Internal tempreature: {}".format(adc_temperature))
     print("Temperature: {}".format(dht11_temp))
     print("Humidity: {}".format(dht11_hum))
---
+++ ds1307.py
@@ -0,0 +1,37 @@
+from micropython import const
+
+DATETIME_REG = const(0) # 0x00-0x06
+CHIP_HALT    = const(128)
+CONTROL_REG  = const(7) # 0x07
+RAM_REG      = const(8) # 0x08-0x3F
+
+class DS1307(object):
+    
+    def __init__(self, i2c, addr = 0x68):
+        self.i2c = i2c
+        self.addr = addr
+        self.weekday_start = 1 
+        self._halt = False
+    
+    def _dec2bcd(self, value):
+        """Convert decimal to binary coded decimal (BCD) format"""
+        return (value // 10) << 4 | (value % 10)
+
+    def _bcd2dec(self, value):
+        """Convert binary coded decimal (BCD) format to decimal"""
+        return ((value >> 4) * 10) + (value & 0x0F)
+    
+    def PrintTime(self):
+        buf = [0]*7
+        buf = self.i2c.readfrom_mem(self.addr, DATETIME_REG, 7)
+        return f"{self._bcd2dec(buf[6]) + 2000}/{self._bcd2dec(buf[5])}/{self._bcd2dec(buf[4])}\n{self._bcd2dec(buf[2])}:{self._bcd2dec(buf[1])}:{self._bcd2dec(buf[0])}"
+        
+        
+    def DateTime(self, datetime = None):
+        """Get or set datetime"""
+        if datetime is None:
+            buf = [0]*7
+            buf = self.i2c.readfrom_mem(self.addr, DATETIME_REG, 7)
+            return(buf)
+        else:
+            self.i2c.writeto_mem(self.addr, DATETIME_REG, datetime)
