da225589c75d3a72494ccf223b838c7f76b604e2
Beszti85 <csababesztercei@gmail.com>
2023-08-02 20:10:54 +0200

    Update ESP32 demo

--- Esp32/main_dht.py
+++ Esp32/main_dht.py
@@ -2,9 +2,23 @@
 import machine
 import time
 import dht
+from machine import Pin
+from lcd_pcf8574 import I2cLcd
 
-led_pin = machine.Pin(2, machine.Pin.OUT)
-sensor_dht11 = dht.DHT11(machine.Pin(0))
+print("Start")
+
+led_pin = Pin(2, Pin.OUT)
+sensor_dht11 = dht.DHT11(Pin(0))
+#i2c display
+scl_pin = Pin(22)
+sda_pin = Pin(21)
+pwm_lcd = machine.PWM(Pin(4))
+
+pwm_lcd.freq(1000)
+pwm_lcd.duty(512)
+
+i2c_board = machine.I2C(sda = sda_pin, scl = scl_pin, freq = 100000)
+lcd_i2c = I2cLcd(i2c_board, 0x20)
 
 while True:
     led_pin.value(1)
@@ -18,3 +32,7 @@ while True:
     dht11_hum = (sensor_dht11.humidity())
     print("Temperature: {}".format(dht11_temp))
     print("Humidity: {}".format(dht11_hum))
+    int_val  = i2c_board.readfrom(0x20, 1)[0]
+    print(int_val)
+    print(i2c_board.readfrom(0x20, 1))
+    lcd_i2c.toggle_led_yellow()
