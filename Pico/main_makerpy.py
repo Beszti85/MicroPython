da471f3ff7809446c9d7ceb477f83deefd88f905
Beszti85 <csababesztercei@gmail.com>
2023-12-16 17:13:44 +0100

    [MakerPi]: correct pin configuration

--- Pico/main_makerpy.py
+++ Pico/main_makerpy.py
@@ -4,7 +4,7 @@ import time
 import utime
 
 # ESP01 on UART0, GP16 and GP17
-uart0 = UART(0, baudrate=115200, tx=Pin(16), rx=Pin(5))
+uart0 = UART(0, baudrate=115200, tx=Pin(16), rx=Pin(17))
 # LED on Pico board
 ledpin = Pin(25, Pin.OUT)
 # Pushbuttons
@@ -17,6 +17,11 @@ led_np = NeoPixel(pin_NP, 1)
 # ADC inputs: GP26 and GP27
 adc_0 = ADC(Pin(26))
 adc_1 = ADC(Pin(27))
+# I2C OLED Display
+i2c=I2C(1,sda=Pin(6), scl=Pin(7), freq=400000)
+
+# Short delay to stop I2C falling over
+time.sleep(1) 
 
 while True:
     ledpin.toggle()
