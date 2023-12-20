a5a221232f7f41a6864ddc1ffcf1c6c6138f7704
Beszti85 <csababesztercei@gmail.com>
2023-12-20 09:42:03 +0100

    [NeoPixel]: added demo for board neopixel led

--- Pico/main_makerpy.py
+++ Pico/main_makerpy.py
@@ -1,4 +1,4 @@
-from machine import Pin, UART, I2C, SPI, ADC
+from machine import Pin, UART, I2C, SPI, ADC, Timer
 from neopixel import NeoPixel
 import time
 import utime
@@ -19,9 +19,27 @@ adc_0 = ADC(Pin(26))
 adc_1 = ADC(Pin(27))
 # I2C OLED Display
 i2c=I2C(1,sda=Pin(6), scl=Pin(7), freq=400000)
+# Timer for neopixel led
+np_timer = Timer()
+np_cycle = 0
 
 # Short delay to stop I2C falling over
-time.sleep(1) 
+time.sleep(1)
+
+def np_change(timer):
+    global np_cycle
+    if np_cycle == 0:
+        led_np[0] = (63, 0, 0)
+        np_cycle = 1
+    elif np_cycle == 1:
+        led_np[0] = (0, 63, 0)
+        np_cycle = 2
+    else:
+        led_np[0] = (0, 0, 63)
+        np_cycle = 0
+    led_np.write()
+
+np_timer.init(mode=Timer.PERIODIC, period=2000, callback=np_change)
 
 while True:
     ledpin.toggle()
