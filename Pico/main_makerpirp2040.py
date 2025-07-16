30f4859013ee9cd6ce75c895a8e2bcc70e7c8654
Beszti85 <csababesztercei@gmail.com>
2025-07-16 22:25:06 +0200

    [MAKERRP2040]: added servo and adc ports

--- Pico/main_makerpirp2040.py
+++ Pico/main_makerpirp2040.py
@@ -6,15 +6,29 @@ import os
 #from ds1307 import DS1307
 import time
 import utime
+import _thread
 
 led_dbg = Pin(16, Pin.OUT)
+#servo pins
+servo12 = PWM(Pin(12))
+servo13 = PWM(Pin(13))
+servo14 = PWM(Pin(14))
 servo15 = PWM(Pin(15))
+#push buttons
+button20 = Pin(20, Pin.IN)
+#adc ports
+adc26 = ADC(26)
+adc27 = ADC(27)
+adc28 = ADC(28)
+adc_bat = ADC(29)
 
 max_duty = 7864
 min_duty = 1802
 half_duty = int(max_duty/2)
 
 servo15.freq(50)
+#read battery voltage
+print(2 * 3.3 * adc_bat.read_u16() / 65535)
 
 while(1):
     led_dbg.value(1)
