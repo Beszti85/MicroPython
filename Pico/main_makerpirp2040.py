27863b517b93bb16bde5a1180e6382d173b063ce
Beszti85 <csababesztercei@gmail.com>
2026-01-01 14:57:37 +0100

    [MAKERRP2040]: added board handlers and asyncio

--- Pico/main_makerpirp2040.py
+++ Pico/main_makerpirp2040.py
@@ -8,20 +8,33 @@ import time
 import utime
 import _thread
 from hcsr04 import HCSR04
+from pushbutton import PushButton
+from neopixel import NeoPixel
+import asyncio
 
 led_dbg = Pin(16, Pin.OUT)
+# Neopixel led: GP18, 2 leds
+pin_NP = Pin(28, Pin.OUT)
+led_np = NeoPixel(pin_NP, 2)
 #servo pins
 servo12 = PWM(Pin(12))
 servo13 = PWM(Pin(13))
 servo14 = PWM(Pin(14))
 servo15 = PWM(Pin(15))
-#push buttons
-button20 = Pin(20, Pin.IN)
+# Pushbuttons
+button_gp20 = PushButton(0, 5, Pin(20, Pin.IN))
+button_gp21 = PushButton(0, 5, Pin(21, Pin.IN))
+# Timer to refresh button states
+button_timer = Timer()
 #adc ports
 adc26 = ADC(26)
 adc27 = ADC(27)
 adc28 = ADC(28)
-adc_bat = ADC(29)
+# Chip temperature sensor
+sensor_temp = ADC(4)
+conversion_factor = 3.3 / (65535)
+# Pico: ADC3 used for VSYS/3
+adc_bat = ADC(Pin(29))
 # buzzer
 buzzer = PWM(Pin(22))
 buzzer.freq(1000)
@@ -46,6 +59,41 @@ motor1_plus.freq(10000)
 #motor1_minus.duty_u16(0)
 #duty_motor = 15000
 
+def button_refresh(timer):
+    button_gp20.update(button_gp20.pin.value())
+    button_gp21.update(button_gp21.pin.value())
+
+button_timer.init(mode=Timer.PERIODIC, period=20, callback=button_refresh)
+
+async def Task1sec():
+    while True:
+        #read battery voltage
+        battery_voltage = 2 * 3.3 * adc_bat.read_u16() / 65535
+        # Read internal temperature sensor
+        read_adctemp = sensor_temp.read_u16() * conversion_factor
+        adc_temperature = 27 - (read_adctemp - 0.706)/0.001721
+        print(f"Battery voltage: {battery_voltage}V")
+        print("Internal tempreature: {}C".format(adc_temperature))
+        # Put here code to be executed every 1 second
+        await asyncio.sleep(1)
+
+async def Task5sec():
+    while True:
+        # Put here code to be executed every 5 seconds
+        print("5 seconds task")
+        await asyncio.sleep(5)
+
+async def main():
+    # Create tasks
+    asyncio.create_task(Task1sec())
+    asyncio.create_task(Task5sec())
+
+# Run the event loop
+loop = asyncio.get_event_loop()
+loop.create_task(main())
+loop.run_forever()
+
+"""
 while(1):
     distance = sensor_us.distance_cm()
     duty_motor = int(distance * 65535 / 400)
@@ -66,3 +114,4 @@ while(1):
     servo15.duty_u16(max_duty)
     buzzer.duty_u16(0)
     time.sleep(1)
+    """
