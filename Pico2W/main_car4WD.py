71f2e5b5fb2572e177d7b844a35a18b04847ff2f
Beszti85 <csababesztercei@gmail.com>
2026-09-11 22:30:31 +0200

    [4WD]: initial commit for 4WD car

---
+++ Pico2W/main_car4WD.py
@@ -0,0 +1,43 @@
+from machine import Pin, I2C, SPI, ADC, Timer, PWM
+from utime import sleep
+import sys
+import select
+import os
+import sdcard
+
+#Board led
+Led_Brd = Pin("LED", Pin.OUT)
+
+#I2C0: PCF8583 RTC
+i2c0 = I2C(0, sda=Pin(4), scl=Pin(5), freq=400000)
+devices = i2c0.scan()
+if len(devices) == 0:
+  print("No i2c device !")
+else:
+  print('i2c devices found:', devices)
+
+#SD CARD
+# SD card
+sd_cs = Pin(17, Pin.OUT, value = 1)
+sd_spi = SPI(0,
+             baudrate = 1000000,
+             polarity = 0,
+             phase = 0,
+             bits = 8,
+             firstbit = SPI.MSB,
+             sck = Pin(18),
+             mosi = Pin(19),
+             miso = Pin(16))
+
+# Initialize SD card
+vosd = sdcard.SDCard(sd_spi, sd_cs)
+
+print("LED starts flashing...")
+while True:
+    try:
+        Led_Brd.toggle()
+        sleep(1) # sleep 1sec
+    except KeyboardInterrupt:
+        break
+Led_Brd.off()
+print("Finished.")
